00da2bb8a83cd2706cb80415cff9c088580337c1
ramesh02001 <137371718+ramesh02001@users.noreply.github.com>
2023-07-25 12:32:16 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,20 @@
+var res = fetch("https://restcountries.com/v2/all");
+res.then((data)=>data.json()).then((data1)=>{
+    for(var i=0; i<data1.length; i++){
+        console.log(data1[i].name);
+        var div = document.createElement("div");
+        div.innerHTML= `<div class="card" style="width: 18rem;">
+        <img src="${data1[i].flag}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${data1[i].name}</h5>
+          <p class="card-text">${data1[i].region}</p>
+          <p class="card-text">${data1[i].subregion}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>   `
+
+        document.body.append(div);
+    }
+});
+
+
